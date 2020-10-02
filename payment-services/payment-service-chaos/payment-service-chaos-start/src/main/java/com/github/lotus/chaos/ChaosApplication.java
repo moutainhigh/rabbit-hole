3e09d96c99afd4979f0e0c8efc92a8964a0527af
hocgin <hocgin@gmail.com>
2020-10-02 13:39:10 +0800

    Update

--- payment-services/payment-service-chaos/payment-service-chaos-start/src/main/java/com/github/lotus/chaos/ChaosApplication.java
+++ payment-services/payment-service-chaos/payment-service-chaos-start/src/main/java/com/github/lotus/chaos/ChaosApplication.java
@@ -1,7 +1,11 @@
 package com.github.lotus.chaos;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by hocgin on 2020/8/15
@@ -9,10 +13,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  * @author hocgin
  */
+@RestController
 @SpringBootApplication
 public class ChaosApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ChaosApplication.class, args);
+    }
+
+    @ApiOperation("测试")
+    @GetMapping("/worked")
+    @ResponseBody
+    public String worked() {
+        return "worked";
     }
 }
