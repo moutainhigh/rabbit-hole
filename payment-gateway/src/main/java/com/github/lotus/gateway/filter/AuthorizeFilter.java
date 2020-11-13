8bc7f0d5be2df47dc269117f6228ec971b3ab166
hocgin <hocgin@gmail.com>
2020-11-14 03:52:16 +0800

    feature: 功能开发

--- payment-gateway/src/main/java/com/github/lotus/gateway/filter/AuthorizeFilter.java
+++ payment-gateway/src/main/java/com/github/lotus/gateway/filter/AuthorizeFilter.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
@@ -22,12 +23,13 @@ import java.security.Principal;
 public class AuthorizeFilter implements GlobalFilter, Ordered {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        return exchange.getPrincipal().map(Principal::getName).map(username -> {
-            exchange.getRequest().mutate()
-                .header(HeaderConstants.USERNAME, username)
-                .build();
-            return exchange;
-        }).flatMap(chain::filter);
+        return exchange.getPrincipal().map(Principal::getName).defaultIfEmpty("")
+            .map(username -> {
+                if (!StringUtils.isEmpty(username)) {
+                    exchange.getRequest().mutate().header(HeaderConstants.USERNAME, username).build();
+                }
+                return exchange;
+            }).flatMap(chain::filter);
     }
 
     @Override
