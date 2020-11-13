65b86efb4fcc745f0a58f92c8d87b6f489b0018e
hocgin <hocgin@gmail.com>
2020-11-14 04:07:52 +0800

    feature: 功能开发

--- payment-services/payment-service-chaos/payment-service-chaos-start/src/main/java/com/github/lotus/chaos/module/wl/controller/WarehouseController.java
+++ payment-services/payment-service-chaos/payment-service-chaos-start/src/main/java/com/github/lotus/chaos/module/wl/controller/WarehouseController.java
@@ -14,6 +14,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,7 +24,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
 import java.util.List;
 
 /**
