fdb4d5d31e0e773f97d679d8c72b830e29714e40
hocgin <hocgin@gmail.com>
2020-12-02 15:55:56 +0800

    feature: 添加微信公众号

---
+++ payment-services/payment-service-wx/payment-service-wx-start/src/main/java/com/github/lotus/wx/basic/support/handle/SubscriptionHandler.java
@@ -0,0 +1,33 @@
+package com.github.lotus.wx.basic.support.handle;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import me.chanjar.weixin.common.error.WxErrorException;
+import me.chanjar.weixin.common.session.WxSessionManager;
+import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.bean.message.WxMpXmlMessage;
+import me.chanjar.weixin.mp.bean.message.WxMpXmlOutMessage;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+/**
+ * Created by hocgin on 2020/4/25.
+ * email: hocgin@gmail.com
+ *
+ * @author hocgin
+ */
+@Slf4j
+@Component
+@RequiredArgsConstructor(onConstructor = @__(@Lazy))
+public class SubscriptionHandler extends AbstractHandler {
+
+    @Override
+    public WxMpXmlOutMessage handle(WxMpXmlMessage wxMessage, Map<String, Object> context, WxMpService wxMpService, WxSessionManager sessionManager) throws WxErrorException {
+        final String appId = wxMpService.getWxMpConfigStorage().getAppId();
+        final String fromUser = wxMessage.getFromUser();
+
+        return null;
+    }
+}
