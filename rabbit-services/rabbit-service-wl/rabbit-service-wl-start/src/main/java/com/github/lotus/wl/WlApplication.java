d96dd5b8c7f11449faeb6192c6371b787aae860a
hocgin <hocgin@gmail.com>
2020-12-04 20:26:52 +0800

    feature: rabbit-wl

---
+++ rabbit-services/rabbit-service-wl/rabbit-service-wl-start/src/main/java/com/github/lotus/wl/WlApplication.java
@@ -0,0 +1,18 @@
+package com.github.lotus.wl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by hocgin on 2020/8/15
+ * email: hocgin@gmail.com
+ *
+ * @author hocgin
+ */
+@SpringBootApplication
+public class WlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WlApplication.class, args);
+    }
+}
