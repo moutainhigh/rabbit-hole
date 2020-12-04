842652f2206c119c9cc47ea01b58a4518f9b465f
hocgin <hocgin@gmail.com>
2020-12-04 19:57:12 +0800

    feature: rabbit-hole

---
+++ rabbit-services/rabbit-service-wx/rabbit-service-wx-start/src/main/java/com/github/lotus/wx/service/WxIndexService.java
@@ -0,0 +1,12 @@
+package com.github.lotus.wx.service;
+
+/**
+ * Created by hocgin on 2020/12/2
+ * email: hocgin@gmail.com
+ *
+ * @author hocgin
+ */
+public interface WxIndexService {
+
+    String getWxMpQrcodeUrl();
+}
