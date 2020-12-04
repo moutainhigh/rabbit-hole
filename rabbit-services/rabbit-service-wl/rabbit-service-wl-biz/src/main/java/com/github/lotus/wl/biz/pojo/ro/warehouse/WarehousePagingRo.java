e1d696c59dfb592c7c137a5d3b7a9b4117f5e96c
hocgin <hocgin@gmail.com>
2020-12-04 21:40:16 +0800

    feature: 新增查询参数

--- rabbit-services/rabbit-service-wl/rabbit-service-wl-biz/src/main/java/com/github/lotus/wl/biz/pojo/ro/warehouse/WarehousePagingRo.java
+++ rabbit-services/rabbit-service-wl/rabbit-service-wl-biz/src/main/java/com/github/lotus/wl/biz/pojo/ro/warehouse/WarehousePagingRo.java
@@ -1,6 +1,9 @@
 package com.github.lotus.wl.biz.pojo.ro.warehouse;
 
 import in.hocg.boot.mybatis.plus.autoconfiguration.ro.PageRo;
+import io.swagger.annotations.ApiModel;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * Created by hocgin on 2020/11/12
@@ -8,6 +11,9 @@ import in.hocg.boot.mybatis.plus.autoconfiguration.ro.PageRo;
  *
  * @author hocgin
  */
+@Data
+@ApiModel
+@EqualsAndHashCode(callSuper = true)
 public class WarehousePagingRo extends PageRo {
-
+    private Long companyId;
 }
