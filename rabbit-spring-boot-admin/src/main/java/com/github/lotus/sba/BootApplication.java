6b8a0e73b5e1c8c06f0218e4403c035d56ab7305
hocgin <hocgin@gmail.com>
2020-12-05 10:59:47 +0800

    feature: Spring Boot Admin

--- rabbit-spring-boot-admin/src/main/java/com/github/lotus/sba/BootApplication.java
+++ rabbit-spring-boot-admin/src/main/java/com/github/lotus/sba/BootApplication.java
@@ -1,10 +1,8 @@
 package com.github.lotus.sba;
 
-import com.github.lotus.common.constant.GlobalConstant;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * Created by hocgin on 2020/12/5
@@ -14,7 +12,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  */
 @EnableAdminServer
 @SpringBootApplication
-@EnableFeignClients(basePackages = GlobalConstant.DEFAULT_FEIGN_BASE_PACKAGE)
 public class BootApplication {
 
     public static void main(String[] args) {
