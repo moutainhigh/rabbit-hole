34e790e61b666507cf5b813da73ba3b6e8815d29
hocgin <hocgin@gmail.com>
2021-01-20 11:39:03 +0800

    feat: 新增配置

--- rabbit-gateway/src/main/java/com/github/lotus/gateway/filter/authorize/AuthorizeProperties.java
+++ rabbit-gateway/src/main/java/com/github/lotus/gateway/filter/authorize/AuthorizeProperties.java
@@ -1,12 +1,11 @@
 package com.github.lotus.gateway.filter.authorize;
 
-import com.google.common.collect.Lists;
 import lombok.Data;
 import lombok.ToString;
 import lombok.experimental.Accessors;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -18,17 +17,16 @@ import java.util.List;
 @Data
 @Accessors(chain = true)
 @ToString
-@Configuration
 @ConfigurationProperties(prefix = AuthorizeProperties.PREFIX)
 public class AuthorizeProperties {
-    public static final String PREFIX = "gateway.authorize";
+    public static final String PREFIX = "rabbit.gateway.authorize";
     /**
      * 忽略的IP列表
      */
-    private List<String> ignoreIps = Lists.newArrayList();
+    private List<String> ignoreIps = Collections.emptyList();
 
     /**
      * 忽略的URI列表
      */
-    private List<String> ignoreUris = Lists.newArrayList();
+    private List<String> ignoreUrls = Collections.emptyList();
 }
