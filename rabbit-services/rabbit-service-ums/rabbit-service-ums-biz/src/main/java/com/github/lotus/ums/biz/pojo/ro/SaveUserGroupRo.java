ffb14f6ee576add40f2d0d5e356b268e82e3fc33
hocgin <hocgin@gmail.com>
2021-01-22 16:47:36 +0800

    feat: 新增接口

--- rabbit-services/rabbit-service-ums/rabbit-service-ums-biz/src/main/java/com/github/lotus/ums/biz/pojo/ro/SaveUserGroupRo.java
+++ rabbit-services/rabbit-service-ums/rabbit-service-ums-biz/src/main/java/com/github/lotus/ums/biz/pojo/ro/SaveUserGroupRo.java
@@ -1,9 +1,12 @@
 package com.github.lotus.ums.biz.pojo.ro;
 
+import in.hocg.boot.validation.autoconfigure.group.Insert;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.experimental.Accessors;
+
+import javax.validation.constraints.NotNull;
 
 /**
  * Created by hocgin on 2021/1/22
@@ -15,6 +18,19 @@ import lombok.experimental.Accessors;
 @Accessors(chain = true)
 @ApiModel
 public class SaveUserGroupRo {
+    @ApiModelProperty("名称")
+    @NotNull(message = "名称不能为空", groups = {Insert.class})
+    private String title;
+    @NotNull(message = "编码不能为空", groups = {Insert.class})
+    @ApiModelProperty("编码")
+    private String encoding;
+    @ApiModelProperty("描述")
+    private String remark;
+    @ApiModelProperty("启用状态")
+    private String enabled;
+    @ApiModelProperty("是否保留")
+    private Boolean isPersist;
+
     @ApiModelProperty(value = "创建者", hidden = true)
     private Long userId;
 }
