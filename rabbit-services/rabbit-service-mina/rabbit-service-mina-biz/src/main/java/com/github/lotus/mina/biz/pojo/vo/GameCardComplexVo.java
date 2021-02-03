274061fea7533052dea3dbda1dafb27ca56978ad
hocgin <hocgin@gmail.com>
2021-02-04 00:49:02 +0800

    feat: update

--- rabbit-services/rabbit-service-mina/rabbit-service-mina-biz/src/main/java/com/github/lotus/mina/biz/pojo/vo/GameCardComplexVo.java
+++ rabbit-services/rabbit-service-mina/rabbit-service-mina-biz/src/main/java/com/github/lotus/mina/biz/pojo/vo/GameCardComplexVo.java
@@ -1,9 +1,15 @@
 package com.github.lotus.mina.biz.pojo.vo;
 
+import com.github.lotus.chaos.api.ChaosNamedAPI;
+import com.github.lotus.chaos.api.NamedType;
+import in.hocg.boot.named.autoconfiguration.annotation.Named;
+import in.hocg.boot.named.autoconfiguration.annotation.UseNamedService;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
+import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
 
@@ -23,4 +29,22 @@ public class GameCardComplexVo {
     private String gameUrl;
     private List<String> tags = Collections.emptyList();
     private List<String> viewUrls = Collections.emptyList();
+    private Integer priority;
+    @ApiModelProperty("启用状态")
+    private Boolean enabled;
+
+    @ApiModelProperty("创建者")
+    private Long creator;
+    @UseNamedService(ChaosNamedAPI.class)
+    @Named(idFor = "creator", type = NamedType.Userid2Nickname)
+    private String creatorName;
+    @ApiModelProperty("创建时间")
+    private LocalDateTime createdAt;
+    @ApiModelProperty("更新者")
+    private Long lastUpdater;
+    @UseNamedService(ChaosNamedAPI.class)
+    @Named(idFor = "lastUpdater", type = NamedType.Userid2Nickname)
+    private String lastUpdaterName;
+    @ApiModelProperty("更新时间")
+    private LocalDateTime lastUpdatedAt;
 }
