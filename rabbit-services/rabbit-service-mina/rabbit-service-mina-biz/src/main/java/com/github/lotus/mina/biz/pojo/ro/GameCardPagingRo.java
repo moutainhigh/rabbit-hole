5450f1bf46f0617eee6dc67b7313e13655f0452b
hocgin <hocgin@gmail.com>
2021-06-12 13:41:04 +0800

    feat: 调整

--- rabbit-services/rabbit-service-mina/rabbit-service-mina-biz/src/main/java/com/github/lotus/mina/biz/pojo/ro/GameCardPagingRo.java
+++ rabbit-services/rabbit-service-mina/rabbit-service-mina-biz/src/main/java/com/github/lotus/mina/biz/pojo/ro/GameCardPagingRo.java
@@ -1,6 +1,10 @@
 package com.github.lotus.mina.biz.pojo.ro;
 
 import in.hocg.boot.mybatis.plus.autoconfiguration.ro.PageRo;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
 
 /**
  * Created by hocgin on 2021/2/3
@@ -8,6 +12,10 @@ import in.hocg.boot.mybatis.plus.autoconfiguration.ro.PageRo;
  *
  * @author hocgin
  */
+@Data
+@Accessors(chain = true)
+@EqualsAndHashCode(callSuper = true)
 public class GameCardPagingRo extends PageRo {
-
+    private String keyword;
+    private String type;
 }
