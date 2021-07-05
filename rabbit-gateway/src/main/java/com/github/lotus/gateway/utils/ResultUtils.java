add437a085317a89246c4b36d156e0c3d9bd2e4e
hocgin <hocgin@gmail.com>
2021-07-05 22:26:14 +0800

    feat: 错误信息打印

--- rabbit-gateway/src/main/java/com/github/lotus/gateway/utils/ResultUtils.java
+++ rabbit-gateway/src/main/java/com/github/lotus/gateway/utils/ResultUtils.java
@@ -31,7 +31,7 @@ public class ResultUtils {
      */
     public Mono<Void> handleException(ServerWebExchange exchange, Exception e) {
         if (CommonUtils.isAjaxRequest(exchange)) {
-            return result(exchange, Result.fail(e.getMessage()));
+            return result(exchange, HttpStatus.OK, Result.fail(e.getMessage()));
         }
         throw new RuntimeException(e);
     }
@@ -44,7 +44,7 @@ public class ResultUtils {
      */
     public Mono<Void> accessDenied(ServerWebExchange exchange) {
         if (CommonUtils.isAjaxRequest(exchange)) {
-            return result(exchange, Result.fail("不具备访问权限"));
+            return result(exchange, HttpStatus.OK, Result.fail("不具备访问权限"));
         }
         log.warn("暂未配置[被拒绝访问, 没有权限]的页面");
         throw new RuntimeException("暂未配置[被拒绝访问, 没有权限]的页面");
