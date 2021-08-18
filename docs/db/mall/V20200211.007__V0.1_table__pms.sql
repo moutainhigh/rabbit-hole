e0b3867af5de365eadce2070da357ec45615421a
hocgin <hocgin@gmail.com>
2021-08-18 16:04:44 +0800

    feat: 调整

---
+++ docs/db/mall/V20200211.007__V0.1_table__pms.sql
@@ -0,0 +1,95 @@
+DROP TABLE IF EXISTS pms_product;
+CREATE TABLE pms_product
+(
+    id                  BIGINT AUTO_INCREMENT,
+    product_category_id BIGINT       NOT NULL
+        COMMENT '商品品类(t_product_classify)',
+    --
+    title               VARCHAR(255) NOT NULL
+        COMMENT '产品名',
+    attrs               JSON         NOT NULL DEFAULT '{}'
+        COMMENT '产品属性: {}',
+    published_flag      TINYINT      NOT NULL DEFAULT 1
+        COMMENT '上架状态',
+    delete_flag         BIGINT       NOT NULL DEFAULT 0
+        COMMENT '删除状态: != 0',
+    video_url           VARCHAR(255)
+        COMMENT 'video url',
+    procurement         VARCHAR(255)
+        COMMENT '采购地(中国,福建)',
+    unit                VARCHAR(16)           DEFAULT '件'
+        COMMENT '单位',
+    weight              DECIMAL(10, 2)        DEFAULT NULL
+        COMMENT '商品重量(克)',
+    --
+    creator             BIGINT,
+    created_at          DATETIME(6)  NOT NULL,
+    last_updater        BIGINT       NULL,
+    last_updated_at     DATETIME(6)  NULL,
+    PRIMARY KEY (id)
+)
+    ENGINE = InnoDB
+    DEFAULT CHARSET = utf8mb4
+    COMMENT '[Shop模块] 商品表';
+--
+DROP TABLE IF EXISTS `pms_product_category`;
+CREATE TABLE pms_product_category
+(
+    id              bigint AUTO_INCREMENT,
+    --
+    title           varchar(255)        NOT NULL
+        COMMENT '商品品类名称',
+    remark          varchar(1024)
+        COMMENT '商品品类描述',
+    image_url       varchar(255)
+        COMMENT '商品品类图片',
+    keywords        varchar(255)
+        COMMENT '商品品类关键词',
+    priority        INT(10)             NOT NULL DEFAULT 1000
+        COMMENT '排序, 从大到小降序',
+    --
+    `parent_id`     BIGINT
+        COMMENT '父级ID, 顶级为 NULL',
+    `tree_path`     VARCHAR(255)        NOT NULL
+        COMMENT '树路径，组成方式: /父路径/当前ID',
+    `enabled`       TINYINT(1) UNSIGNED NOT NULL DEFAULT 1
+        COMMENT '启用状态',
+    --
+    creator         BIGINT,
+    created_at      DATETIME(6)         NOT NULL,
+    last_updater    BIGINT              NULL,
+    last_updated_at DATETIME(6)         NULL,
+    PRIMARY KEY (`id`)
+)
+    ENGINE = InnoDB
+    DEFAULT CHARSET = utf8mb4
+    COMMENT '[Shop模块] 商品品类表';
+--
+DROP TABLE IF EXISTS `pms_sku`;
+CREATE TABLE `pms_sku`
+(
+    id         BIGINT AUTO_INCREMENT,
+    product_id BIGINT,
+    --
+    encoding   VARCHAR(128)   NOT NULL UNIQUE
+        COMMENT 'SKU 编码',
+    unit_price DECIMAL(10, 2) NOT NULL
+        COMMENT '单价(如: 12.00)',
+    stock      INT            NOT NULL DEFAULT 0
+        COMMENT '库存, 默认为0',
+    sale       INT            NOT NULL DEFAULT 0
+        COMMENT '销量, 默认为0',
+    spec_data  JSON                    DEFAULT '[]'
+        COMMENT '规格属性(JSONArray, 如: [{"key":"颜色","value":"银色"}])',
+    image_url  VARCHAR(255)
+        COMMENT '该特色商品图片',
+    --
+    PRIMARY KEY (id)
+)
+    ENGINE = InnoDB
+    DEFAULT CHARSET = utf8mb4
+    COMMENT '[Shop模块] 商品SKU表';
+
+-- 品类
+INSERT INTO pms_product_category(id, title, tree_path, creator, created_at)
+    VALUE (1, '测试品类', '/1', 1, NOW());
