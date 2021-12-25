40900787df6cfddaac7cc825f049ce361d78355e
hocgin <hocgin@gmail.com>
2021-12-25 23:14:50 +0800

    feat: 调整

--- rabbit-services/rabbit-service-chaos/rabbit-service-chaos-start/src/main/resources/db/migration/v0.0.5/V20211116.001__V0.1_table__dl_channel.sql
+++ rabbit-services/rabbit-service-chaos/rabbit-service-chaos-start/src/main/resources/db/migration/v0.0.5/V20211116.001__V0.1_table__dl_channel.sql
@@ -25,11 +25,11 @@ CREATE TABLE `dl_proxy_channel`
         COMMENT '租户',
     `delete_flag`     BIGINT              NOT NULL DEFAULT 0
         COMMENT '删除状态(未删除 != 0)',
-    `created_at`      DATETIME(6)         NOT NULL
+    `created_at`      DATETIME(6)         NOT NULL DEFAULT CURRENT_TIMESTAMP(6)
         COMMENT '创建时间',
     `creator`         BIGINT
         COMMENT '创建者',
-    `last_updated_at` DATETIME(6)         NULL
+    `last_updated_at` DATETIME(6)         NULL     DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP(6)
         COMMENT '更新时间',
     `last_updater`    BIGINT
         COMMENT '更新者',
