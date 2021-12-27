0a9048e1046f6a8610941f6d733a6c54a3a9acaf
hocgin <hocgin@gmail.com>
2021-12-27 15:31:00 +0800

    feat: frpc

--- scripts/frp/frpc.sh
+++ scripts/frp/frpc.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 path=$(cd `dirname $0`; pwd)
 
-$path/frpc -c $path/frpc.ini
+$path/frpc --id=hocgin
--- scripts/frp/frps.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-path=$(cd `dirname $0`; pwd)
-
-$path/frps -c $path/frps.ini
