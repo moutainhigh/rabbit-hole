3b98f965c9865d87ae903c613b0b4749cab0e1b0
hocgin <hocgin@gmail.com>
2021-12-29 00:48:56 +0800

    feat: cache

--- rabbit-sso-server/src/main/java/in/hocg/rabbit/sso/service/impl/SocialServiceImpl.java
+++ rabbit-sso-server/src/main/java/in/hocg/rabbit/sso/service/impl/SocialServiceImpl.java
@@ -33,14 +33,21 @@ public class SocialServiceImpl implements SocialService {
     @Override
     public void onAuthenticationSuccess(String socialType, String socialId, String username) {
         UserDetailVo userDetail = socialServiceApi.getAccountBySocialTypeAndSocialId(socialType, socialId);
+        // 1. 该社交账户已经绑定，并且当前没有人登录 -> 登录
+        // 2. 该社交账户已经绑定，并且当前登录人不是本人 -> 注销原来的登录，登录新的
+        // 2. 该社交账户已经绑定，并且当前登录人是本人 -> 登录
+        // 3. 该社交账户没有绑定，并且当前没有人登录 -> 跳到注册
+        // 4. 该社交账户没有绑定，并且当前有登录人 -> 绑定
+
 
         // 社交账号未被绑定
         if (Objects.isNull(userDetail)) {
             // 已有用户登陆
             if (Objects.nonNull(username)) {
-                // 这个用户需要未绑定改类型的社交账号
+                // todo 这个用户未绑定该类型的社交账号，少了一个判断
                 log.debug("==> 绑定社交账号 [{}] 到 [{}]", socialType + socialId, username);
                 UserDetailVo user = userServiceApi.getUserByUsername(username);
+                // 绑定用户账户
                 socialServiceApi.insertOne(new InsertSocialRo()
                     .setSocialType(socialType)
                     .setSocialId(socialId)
@@ -48,12 +55,11 @@ public class SocialServiceImpl implements SocialService {
             }
             // 未有用户登陆
             else {
-                // 退出社交账号登陆
                 log.debug("==> 使用社交账号 [{}] 去注册 or 跳转注册页面", socialType + socialId);
+                // 退出社交账号登陆
                 SecurityContext.signOut();
                 try {
-                    SpringServletContext.getResponse()
-                        .orElseThrow(() -> ServiceException.wrap("系统繁忙"))
+                    SpringServletContext.getResponse().orElseThrow(() -> ServiceException.wrap("系统繁忙"))
                         .sendRedirect(PageConstants.SIGN_UP_PAGE);
                 } catch (IOException e) {
                     throw ServiceException.wrap(e);
