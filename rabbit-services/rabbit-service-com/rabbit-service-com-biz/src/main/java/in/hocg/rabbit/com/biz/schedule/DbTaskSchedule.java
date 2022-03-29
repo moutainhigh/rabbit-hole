3a70aa559517485ca7eac5eb5ccdd9f80d87df2b
hocgin <hocgin@gmail.com>
2022-03-29 09:22:50 +0800

    feat: 调整

--- rabbit-services/rabbit-service-com/rabbit-service-com-biz/src/main/java/in/hocg/rabbit/com/biz/schedule/DbTaskSchedule.java
+++ rabbit-services/rabbit-service-com/rabbit-service-com-biz/src/main/java/in/hocg/rabbit/com/biz/schedule/DbTaskSchedule.java
@@ -3,13 +3,10 @@ package in.hocg.rabbit.com.biz.schedule;
 import com.alibaba.schedulerx.worker.domain.JobContext;
 import com.alibaba.schedulerx.worker.processor.JavaProcessor;
 import com.alibaba.schedulerx.worker.processor.ProcessResult;
-import in.hocg.boot.task.autoconfiguration.core.entity.TaskInfo;
-import in.hocg.boot.task.autoconfiguration.core.entity.TaskItem;
+import in.hocg.boot.mybatis.plus.extensions.task.TaskMpeService;
 import in.hocg.rabbit.com.api.enums.TaskType;
 import in.hocg.rabbit.mina.api.YouTubeServiceApi;
 import in.hocg.rabbit.mina.api.pojo.ro.UploadYouTubeRo;
-import in.hocg.boot.task.autoconfiguration.core.TaskBervice;
-import in.hocg.boot.task.autoconfiguration.core.TaskRepository;
 import in.hocg.boot.utils.enums.ICode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,7 +14,6 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
-import java.util.function.Consumer;
 import java.util.function.Function;
 
 /**
@@ -30,13 +26,13 @@ import java.util.function.Function;
 @Component
 @RequiredArgsConstructor(onConstructor = @__(@Lazy))
 public class DbTaskSchedule extends JavaProcessor {
-    private final TaskRepository taskRepository;
-    private final TaskBervice taskBervice;
+    private final TaskMpeService taskMpeService;
     private final YouTubeServiceApi youTubeServiceApi;
 
     @Override
     public ProcessResult process(JobContext context) throws Exception {
-        taskRepository.listByReady().forEach(taskItem -> taskBervice.runAsync(taskItem.getId(), this.getTaskFunction(taskItem.getType())));
+        taskMpeService.listByReady()
+            .forEach(taskItem -> taskMpeService.runAsync(taskItem.getId(), this.getTaskFunction(taskItem.getType())));
         return new ProcessResult(true);
     }
 
