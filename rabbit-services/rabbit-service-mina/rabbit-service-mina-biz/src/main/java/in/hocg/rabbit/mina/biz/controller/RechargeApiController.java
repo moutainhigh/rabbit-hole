70d218ba7cabcc258a30d85fef17ad63f444b78d
hocgin <hocgin@gmail.com>
2022-04-08 14:10:17 +0800

    feat: 充值相关接口

--- rabbit-services/rabbit-service-mina/rabbit-service-mina-biz/src/main/java/in/hocg/rabbit/mina/biz/controller/RechargeApiController.java
+++ rabbit-services/rabbit-service-mina/rabbit-service-mina-biz/src/main/java/in/hocg/rabbit/mina/biz/controller/RechargeApiController.java
@@ -1,17 +1,22 @@
-package in.hocg.rabbit.mina.biz.apiimpl;
+package in.hocg.rabbit.mina.biz.controller;
 
 import in.hocg.boot.utils.struct.result.Result;
-import in.hocg.rabbit.mina.api.RechargeServiceApi;
 import in.hocg.rabbit.mina.api.pojo.ro.BaseRechargeRo;
 import in.hocg.rabbit.mina.api.pojo.ro.QueryRechargeOrderRo;
 import in.hocg.rabbit.mina.api.pojo.ro.RechargeOrderRo;
 import in.hocg.rabbit.mina.api.pojo.vo.RechargeOrderVo;
 import in.hocg.rabbit.mina.api.pojo.vo.RechargeProductVo;
+import in.hocg.rabbit.mina.biz.constant.MinaConstant;
 import in.hocg.rabbit.mina.biz.service.RechargeOrderService;
 import in.hocg.rabbit.mina.biz.support.recharge.RechargeHelper;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -24,24 +29,29 @@ import java.util.List;
  */
 @Validated
 @RestController
+@Api(tags = "mina::充值对外接口")
+@RequestMapping("/api")
 @RequiredArgsConstructor(onConstructor_ = {@Lazy})
-public class RechargeServiceApiImpl implements RechargeServiceApi {
+public class RechargeApiController {
     private final RechargeOrderService rechargeOrderService;
 
-    @Override
-    public Result<RechargeOrderVo> recharge(RechargeOrderRo ro) {
+    @ApiOperation("充值操作")
+    @PostMapping("/recharge")
+    public Result<RechargeOrderVo> recharge(@RequestBody RechargeOrderRo ro) {
         RechargeHelper.checkThrow(ro, ro.getUserId(), ro.getSign());
         return Result.success(rechargeOrderService.recharge(ro));
     }
 
-    @Override
-    public Result<RechargeOrderVo> queryRecharge(QueryRechargeOrderRo ro) {
+    @ApiOperation("充值结果")
+    @PostMapping("/recharge/result")
+    public Result<RechargeOrderVo> queryRecharge(@RequestBody QueryRechargeOrderRo ro) {
         RechargeHelper.checkThrow(ro, ro.getUserId(), ro.getSign());
         return Result.success(rechargeOrderService.queryRecharge(ro));
     }
 
-    @Override
-    public Result<List<RechargeProductVo>> listProduct(BaseRechargeRo ro) {
+    @ApiOperation("充值产品列表")
+    @PostMapping("/recharge/product")
+    public Result<List<RechargeProductVo>> listProduct(@RequestBody BaseRechargeRo ro) {
         RechargeHelper.checkThrow(ro, ro.getUserId(), ro.getSign());
         return Result.success(rechargeOrderService.listProduct(ro.getUserId()));
     }
