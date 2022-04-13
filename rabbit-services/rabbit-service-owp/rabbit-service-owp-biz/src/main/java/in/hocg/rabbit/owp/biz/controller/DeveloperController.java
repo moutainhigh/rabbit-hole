2fb6bbd9f26dd96fc8907e96228dcbfb430ff3e8
hocgin <hocgin@gmail.com>
2022-04-13 10:26:52 +0800

    feat: 修复问题

--- rabbit-services/rabbit-service-owp/rabbit-service-owp-biz/src/main/java/in/hocg/rabbit/owp/biz/controller/DeveloperController.java
+++ rabbit-services/rabbit-service-owp/rabbit-service-owp-biz/src/main/java/in/hocg/rabbit/owp/biz/controller/DeveloperController.java
@@ -1,6 +1,13 @@
 package in.hocg.rabbit.owp.biz.controller;
 
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import in.hocg.boot.utils.struct.result.Result;
+import in.hocg.rabbit.usercontext.autoconfigure.UserContextHolder;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.context.annotation.Lazy;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author hocgin
  * @since 2022-04-10
  */
+@Validated
 @RestController
 @RequiredArgsConstructor(onConstructor = @__(@Lazy))
 @RequestMapping("/developer")
