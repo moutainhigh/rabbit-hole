883e0d4288660851a1425bd7edb2737cc4153bd9
hocgin <hocgin@gmail.com>
2022-04-13 10:15:10 +0800

    feat: 修复问题

--- rabbit-services/rabbit-service-mina/rabbit-service-mina-biz/src/main/java/in/hocg/rabbit/mina/biz/pojo/vo/RechargeProductVo.java
+++ rabbit-services/rabbit-service-mina/rabbit-service-mina-biz/src/main/java/in/hocg/rabbit/mina/biz/pojo/vo/RechargeProductVo.java
@@ -1,10 +1,12 @@
 package in.hocg.rabbit.mina.biz.pojo.vo;
 
 import cn.hutool.core.util.StrUtil;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.StringJoiner;
 
@@ -16,15 +18,20 @@ import java.util.StringJoiner;
  */
 @Data
 @Accessors(chain = true)
-public class RechargeProductVo {
+public class RechargeProductVo implements Serializable {
+    @JsonIgnoreProperties(ignoreUnknown = true)
     @ApiModelProperty("产品ID")
     private String id;
+    @JsonIgnoreProperties(ignoreUnknown = true)
     @ApiModelProperty("类型名称")
     private String className;
+    @JsonIgnoreProperties(ignoreUnknown = true)
     @ApiModelProperty("产品名称")
     private String productName;
+    @JsonIgnoreProperties(ignoreUnknown = true)
     @ApiModelProperty("价格，下单扣费金额")
     private BigDecimal price;
+    @JsonIgnoreProperties(ignoreUnknown = true)
     @ApiModelProperty("备注信息")
     private String desc;
 
