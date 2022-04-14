68ad709ea731f593e12b7e8177b95533c97e45dd
hocgin <hocgin@gmail.com>
2022-04-14 10:20:25 +0800

    feat: 修复问题

--- rabbit-openway/src/test/java/in/hocg/rabbit/openway/filter/ValidRequestFilterTest.java
+++ rabbit-openway/src/test/java/in/hocg/rabbit/openway/filter/ValidRequestFilterTest.java
@@ -26,12 +26,15 @@ import java.util.Map;
  * @author hocgin
  */
 class ValidRequestFilterTest {
+    String APP_ID = "--使用你的APPID--";
+    String SECRET_KEY = "--使用你的 SECRET KEY--";
 
+    @Test
     public void queryProduct() {
         System.out.println("查询产品------------------");
         RequestBody.SignType signType = RequestBody.SignType.MD5;
         ToRequestBody body = new ToRequestBody();
-        body.setAppid("all_test");
+        body.setAppid(APP_ID);
         String method = "ER0003";
         body.setMethod(method);
         body.setSignType(signType.getType().toLowerCase());
@@ -39,7 +42,7 @@ class ValidRequestFilterTest {
         body.setBizContent(new HashMap<>() {{
         }});
         String json = JSONUtil.toJsonStr(body);
-        body.setSign(OpenwayUtils.getSign(json, signType, "hocgin"));
+        body.setSign(OpenwayUtils.getSign(json, signType, SECRET_KEY));
 
         String reqBody = JSONUtil.toJsonStr(body);
         String resp = HttpUtil.post("https://openapi.hocgin.top", reqBody);
@@ -55,16 +58,16 @@ class ValidRequestFilterTest {
         System.out.println("查询充值结果------------------");
         RequestBody.SignType signType = RequestBody.SignType.MD5;
         ToRequestBody body = new ToRequestBody();
-        body.setAppid("all_test");
+        body.setAppid(APP_ID);
         String method = "ER0002";
         body.setMethod(method);
         body.setSignType(signType.getType().toLowerCase());
         body.setTimestamp(LocalDateTime.now());
         body.setBizContent(new HashMap<>() {{
-            put("outOrderNo", "9gxtqehm1ivb2z5m");
+            put("outOrderNo", "--填写单号--");
         }});
         String json = JSONUtil.toJsonStr(body);
-        body.setSign(OpenwayUtils.getSign(json, signType, "hocgin"));
+        body.setSign(OpenwayUtils.getSign(json, signType, SECRET_KEY));
 
         String reqBody = JSONUtil.toJsonStr(body);
         String resp = HttpUtil.post("https://openapi.hocgin.top", reqBody);
@@ -79,20 +82,20 @@ class ValidRequestFilterTest {
     public void recharge() {
         RequestBody.SignType signType = RequestBody.SignType.MD5;
         ToRequestBody body = new ToRequestBody();
-        body.setAppid("all_test");
+        body.setAppid(APP_ID);
         String method = "ER0001";
         body.setMethod(method);
         body.setSignType(signType.getType().toLowerCase());
         body.setTimestamp(LocalDateTime.now());
         body.setBizContent(new HashMap<>() {{
             put("outOrderNo", "TEST_" + System.currentTimeMillis());
-            put("productId", "5");
-            put("account", "13600747016");
+            put("productId", "--填写产品ID--");
+            put("account", "--填写号码--");
             put("maxCostAmt", null);
             put("notifyUrl", null);
         }});
         String json = JSONUtil.toJsonStr(body);
-        body.setSign(OpenwayUtils.getSign(json, signType, "hocgin"));
+        body.setSign(OpenwayUtils.getSign(json, signType, SECRET_KEY));
 
         String reqBody = JSONUtil.toJsonStr(body);
         String resp = HttpUtil.post("https://openapi.hocgin.top", reqBody);
@@ -104,6 +107,7 @@ class ValidRequestFilterTest {
     }
 
 
+    // 解析消息
     public static Pair<String, String> getBody(String body, String method) {
         Map<String, String> result = JSONUtil.toBean(body, new TypeReference<>() {
         }, true);
@@ -111,6 +115,22 @@ class ValidRequestFilterTest {
         String searchStr = "\"" + method + "\":";
         int startIndex = StrUtil.ordinalIndexOf(body, searchStr, 1) + searchStr.length();
         return Pair.of(StrUtil.subWithLength(body, startIndex, respBody.length()), result.get(OpenwayContants.SIGN));
+    }
+
+    public String getSign(String requestBody, RequestBody.SignType signType, String secretKey) {
+        JSONObject params = JSONUtil.toBean(requestBody, JSONObject.class);
+        String signStr = params.keySet().stream().sorted(Comparator.comparing(o -> o))
+            .filter(StrUtil::isNotBlank)
+            .filter(s -> !"sign".equals(s))
+            .map(s -> StrUtil.format("{}={}", s, ObjectUtil.defaultIfNull(params.get(s), "")))
+            .reduce((a, b) -> StrUtil.format("{}&{}", a, b))
+            .map(s -> s + "&secretKey=" + secretKey)
+            .orElseThrow();
+        return getSignStr(signStr);
+    }
+
+    public String getSignStr(String str) {
+        return SecureUtil.md5(str).toLowerCase();
     }
 
 }
