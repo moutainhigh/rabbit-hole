47d94285b809def5dccba638d323629bbc479479
hocgin <hocgin@gmail.com>
2022-06-20 16:52:36 +0800

    feat: 调整

---
+++ rabbit-services/rabbit-service-ws/rabbit-service-ws-biz/src/main/java/in/hocg/rabbit/ws/biz/service/GameService.java
@@ -0,0 +1,16 @@
+package in.hocg.rabbit.ws.biz.service;
+
+import in.hocg.rabbit.ws.biz.pojo.ro.GameCmdRo;
+import in.hocg.rabbit.ws.biz.pojo.ro.RoomSignalRo;
+
+/**
+ * Created by hocgin on 2022/6/8
+ * email: hocgin@gmail.com
+ *
+ * @author hocgin
+ */
+public interface GameService {
+    void handleRoomRequest(GameCmdRo ro);
+
+    void handleRoomSignal(RoomSignalRo ro, String username);
+}
