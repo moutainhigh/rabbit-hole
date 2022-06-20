496e65cbe7aad534bf2991e2945239345a656230
hocgin <hocgin@gmail.com>
2022-06-20 15:50:50 +0800

    feat: 调整

--- rabbit-gateway/src/main/java/in/hocg/rabbit/gateway/filter/authentication/AuthenticationFilter.java
+++ rabbit-gateway/src/main/java/in/hocg/rabbit/gateway/filter/authentication/AuthenticationFilter.java
@@ -1,9 +1,11 @@
 package in.hocg.rabbit.gateway.filter.authentication;
 
+import in.hocg.rabbit.gateway.utils.ResultUtils;
 import in.hocg.rabbit.usercontext.basic.HeaderConstants;
 import in.hocg.boot.sso.client.autoconfigure.utils.TokenUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.reactivestreams.Publisher;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Lazy;
@@ -14,6 +16,7 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 import java.security.Principal;
+import java.util.function.Function;
 
 /**
  * Created by hocgin on 2020/9/6
@@ -29,12 +32,15 @@ public class AuthenticationFilter implements GlobalFilter, Ordered {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         return exchange.getPrincipal().map(Principal::getName).defaultIfEmpty("")
-            .map(username -> {
+            .flatMap(username -> {
+                if (isExpiredToken(username)) {
+                    return ResultUtils.expired(exchange);
+                }
                 if (!isAnonymous(username)) {
                     exchange.getRequest().mutate().header(HeaderConstants.USERNAME, username).build();
                 }
-                return exchange;
-            }).flatMap(chain::filter);
+                return chain.filter(exchange);
+            });
     }
 
     @Override
@@ -46,4 +52,7 @@ public class AuthenticationFilter implements GlobalFilter, Ordered {
         return StringUtils.isEmpty(username) || TokenUtils.ANONYMOUS_AUTHENTICATION_TOKEN.getPrincipal().equals(username);
     }
 
+    private boolean isExpiredToken(String username) {
+        return AuthenticationTokenFilter.EXPIRED_TOKEN.equals(username);
+    }
 }
