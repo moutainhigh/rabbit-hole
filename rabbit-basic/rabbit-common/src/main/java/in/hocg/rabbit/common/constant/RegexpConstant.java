a9f6ac18e2c61146cbc0a1c1da1c83f886539951
hocgin <hocgin@gmail.com>
2022-09-01 21:16:47 +0800

    update

--- rabbit-basic/rabbit-common/src/main/java/in/hocg/rabbit/common/constant/RegexpConstant.java
+++ rabbit-basic/rabbit-common/src/main/java/in/hocg/rabbit/common/constant/RegexpConstant.java
@@ -12,6 +12,6 @@ import lombok.experimental.UtilityClass;
 public class RegexpConstant {
     public static final String USERNAME = "^\\w{6,12}$";
     public static final String PASSWORD = "^.{6,32}$";
-    public static final String VERIFY_CODE = "^\\d{6}$";
+    public static final String VERIFY_CODE = "^\\d{4}$";
     public static final String PHONE = "^(0|86|17951)?(13[0-9]|15[012356789]|166|17[3678]|18[0-9]|14[57])[0-9]{8}$";
 }
